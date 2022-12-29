310c125410a2309b18acdcf3d7f6c8b1b6b690ec
sungvzer <sungvzer@proton.me>
2022-12-29 14:16:15 +0100

    chore: initial commit

---
+++ lib/Terminale.h
@@ -0,0 +1,18 @@
+/**
+ * Questo file contiene le definizioni per le necessarie alla stampa su
+ * terminale
+ * Salvatore Gargano <salvatore.gargano001@studenti.uniparthenope.it>
+ */
+#pragma once
+
+/**
+ * I vari colori ANSI utilizzabili nel terminale
+ */
+#define ANSI_RESET "\x1b[0m"
+#define ANSI_NERO "\x1b[30m"
+#define ANSI_ROSSO "\x1b[31m"
+#define ANSI_VERDE "\x1b[32m"
+#define ANSI_GIALLO "\x1b[33m"
+#define ANSI_BLU "\x1b[34m"
+#define ANSI_MAGENTA "\x1b[35m"
+#define ANSI_CIANO "\x1b[36m"
---
+++ src/main.c
@@ -0,0 +1,10 @@
+/**
+ * Salvatore Gargano <salvatore.gargano001@studenti.uniparthenope.it>
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  printf("Hello world!\n");
+  return 0;
+}
